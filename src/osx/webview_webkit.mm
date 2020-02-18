dbd3429001db8cbd0195902d5d9e9bd08b75025f
Jon Evans <jon@craftyjon.com>
2020-02-18 11:36:53 -0500

    Fix build on 10.15

--- src/osx/webview_webkit.mm
+++ src/osx/webview_webkit.mm
@@ -29,8 +29,10 @@
 #include "wx/filesys.h"
 
 #include <WebKit/WebKit.h>
+#if MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_15
 #include <WebKit/HIWebView.h>
 #include <WebKit/CarbonUtils.h>
+#endif
 
 #include <Foundation/NSURLError.h>
 
