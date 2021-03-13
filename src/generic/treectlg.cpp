37d27a780dd2b6c54837602b3aa815574cee00e1
Jeff Young <jeff@rokeby.ie>
2021-03-13 11:45:25 +0000

    Improve tree control spacing.
    Note that this *might* need ifdef-ing for GTK & MSW if it were to
    be upstreamed.

--- src/generic/treectlg.cpp
+++ src/generic/treectlg.cpp
@@ -981,8 +981,8 @@ void wxGenericTreeCtrl::Init()
     m_dirty = false;
 
     m_lineHeight = 10;
-    m_indent = 15;
-    m_spacing = 18;
+    m_indent = 10;
+    m_spacing = 15;
 
     m_dragCount = 0;
     m_isDragging = false;
