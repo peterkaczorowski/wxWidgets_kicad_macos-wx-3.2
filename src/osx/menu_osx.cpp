87c32e2edb1e9c0b16781333c00f61e3cab37af5
Jon Evans <jon@craftyjon.com>
2022-12-13 12:05:44 -0500

    wxMenu::HandleCommandProcess: null pointer safety
    See https://gitlab.com/kicad/code/kicad/-/issues/13149

--- src/osx/menu_osx.cpp
+++ src/osx/menu_osx.cpp
@@ -363,7 +363,7 @@ bool wxMenu::HandleCommandProcess( wxMenuItem* item )
 
     int menuid = item->GetId();
     bool processed = false;
-    if (item->IsCheckable())
+    if (item && item->IsCheckable())
         item->Check( !item->IsChecked() ) ;
 
     // A bit counterintuitively, we call OSXAfterMenuEvent() _before_ calling
@@ -375,7 +375,7 @@ bool wxMenu::HandleCommandProcess( wxMenuItem* item )
         w->OSXAfterMenuEvent();
     }
 
-    if ( SendEvent( menuid , item->IsCheckable() ? item->IsChecked() : -1 ) )
+    if ( SendEvent( menuid , item && item->IsCheckable() ? item->IsChecked() : -1 ) )
         processed = true ;
 
     if(!processed)
