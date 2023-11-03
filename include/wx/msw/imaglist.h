74c23b4abb61a05b56211a86b1eb39e94bd4d116
Vadim Zeitlin <vadim@wxwidgets.org>
2023-11-03 01:36:00 +0100

    Replace wxImageList::Init() with member initializers in wxMSW
    No real change, just initialize members in their declarations and get
    rid of now unnecessary Init().
    
    (cherry picked from commit 0003ac6873c754126375295dd0052017df6fb367)

--- include/wx/msw/imaglist.h
+++ include/wx/msw/imaglist.h
@@ -20,7 +20,7 @@ public:
    * Public interface
    */
 
-  wxImageList() { Init(); }
+  wxImageList() = default;
 
   // Creates an image list.
   // Specify the width and height of the images in the list,
@@ -28,7 +28,6 @@ public:
   // from icons), and the initial size of the list.
   wxImageList(int width, int height, bool mask = true, int initialCount = 1)
   {
-    Init();
     Create(width, height, mask, initialCount);
   }
   virtual ~wxImageList();
@@ -192,7 +191,7 @@ public:
   WXHIMAGELIST GetHIMAGELIST() const { return m_hImageList; }
 
 protected:
-  WXHIMAGELIST m_hImageList;
+  WXHIMAGELIST m_hImageList = nullptr;
   wxSize m_size;
 
 private:
@@ -204,13 +203,7 @@ private:
   void GetImageListBitmaps(wxMSWBitmaps& bitmaps,
                            const wxBitmap& bitmap, const wxBitmap& mask);
 
-  bool m_useMask;
-
-  void Init()
-  {
-    m_hImageList = NULL;
-    m_useMask = false;
-  }
+  bool m_useMask = false;
 
   wxDECLARE_DYNAMIC_CLASS_NO_COPY(wxImageList);
 };
