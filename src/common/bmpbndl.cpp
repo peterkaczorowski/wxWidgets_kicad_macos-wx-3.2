12357eafb3e31f9a7bf39d6a84ef8c50e3fcb749
Vadim Zeitlin <vadim@wxwidgets.org>
2023-11-03 02:17:33 +0100

    Use physical size in wxImageList
    This seems to be the only way to fix using this class, which is
    fundamentally incompatible with high DPI support, with bitmaps of
    different logical but identical physical sizes.
    
    By using physical size we ensure that the code chopping up the provided
    bitmap in multiple images doesn't do it with the bitmap having the same
    physical size but a different logical size from all the other bitmaps.
    And, conceptually, logical size of the bitmaps in it really shouldn't
    matter to the image list as it doesn't have a scaling factor and so
    can't do anything useful with the logical size.
    
    Moreover, we can't add wxImageList::SetScaleFactor() neither because
    this class is, by design, supposed to be shareable between different
    controls which may use different scale factors, so it doesn't make sense
    to speak of the scale factor of the image list itself.
    
    This undoes the changes made in the generic implementation back in
    c374eefd34 (Fold wxOSX-specific wxImageList into generic version,
    2018-10-30) and also eb52e86553 (Take into account scale factor of
    images added to wxImageList, 2021-04-03).
    
    Also update a couple of places where wxImageList is still used to use
    physical sizes. Unfortunately some others can't be easily fixed, e.g.
    global wxFileIconsTable would have to be completely rewritten.
    
    Finally, add a unit test checking that things work as expected now:
    previously the size of 24x24 bitmap in the image list containing
    (scaled) 32x32 bitmaps would be 21x21 due to sub-bitmap extraction
    kicking in.
    
    Closes #23994.
    
    (cherry picked from commit 53b4f4ddf23285cba3fc3243632eae1b047b9831)

--- src/common/bmpbndl.cpp
+++ src/common/bmpbndl.cpp
@@ -705,11 +705,7 @@ wxBitmapBundle::CreateImageList(wxWindow* win,
     wxCHECK_MSG( win, NULL, "must have a valid window" );
     wxCHECK_MSG( !bundles.empty(), NULL, "should have some images" );
 
-    wxSize size = GetConsensusSizeFor(win, bundles);
-
-    // wxImageList wants the logical size for the platforms where logical and
-    // physical pixels are different.
-    size /= win->GetContentScaleFactor();
+    const wxSize size = GetConsensusSizeFor(win, bundles);
 
     wxImageList* const iml = new wxImageList(size.x, size.y);
 
